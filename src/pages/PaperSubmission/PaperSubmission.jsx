16d047435798db8620f93cacf32451caafcd8319
sourabh-mourya <sourabhwork049@gmail.com>
2026-08-21 15:54:29 +0530

    feat(paper-submission): add Microsoft CMT service acknowledgment note under CMT button

--- src/pages/PaperSubmission/PaperSubmission.jsx
+++ src/pages/PaperSubmission/PaperSubmission.jsx
@@ -188,6 +188,12 @@ const PaperSubmission = () => {
                 <i className="fa-solid fa-circle-info"></i> Paper Submission Portal Will Open Soon
               </div>
             )}
+
+            <div className="cmt-ack-note">
+              <p className="cmt-ack-text">
+                The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
+              </p>
+            </div>
           </div>
 
         </div>
